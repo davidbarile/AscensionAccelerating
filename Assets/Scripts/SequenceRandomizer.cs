408cf9e6fc9999212e6ce010e6c56d5c6dd5d364
David Barile <davidbarile@gmail.com>
2026-06-09 09:30:39 -0700

    wip: adding unique check to randomize algorithm

--- Assets/Scripts/SequenceRandomizer.cs
+++ Assets/Scripts/SequenceRandomizer.cs
@@ -1,25 +1,80 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class SequenceRandomizer : MonoBehaviour
 {
+    [SerializeField] private int maxPatterns = 5;
     [SerializeField] private SequenceTab openingSequenceTab;
     [SerializeField] private SequenceTab[] sequenceTabs;
 
     private int[] openingSequence = { 5, 4, 3, 2, 1 };//set custom order here
     private int[] numbers = { 0, 1, 2, 3, 4, 5 };
 
-    public void GenerateRandomPatterns()
+    public void SetOpeningPattern()
     {
         var openingSequence = this.openingSequence.ToList();
         this.openingSequenceTab.SetPatterns(openingSequence);
+    }
 
-        var randomNums = this.numbers.ToList();
+    public void GenerateRandomPatterns()
+    {
+        SetOpeningPattern();
+
         foreach(var tab in this.sequenceTabs)
         {
+            tab.Numbers.Clear();
+        }
+
+        var randomNums = this.numbers.ToList();
+
+        foreach (var tab in this.sequenceTabs)
+        {
             randomNums.RandomizeList();
-            tab.SetPatterns(randomNums);
+            tab.SetNumbers(randomNums);
+            //tab.SetPatterns(randomNums);
+        }
+        
+        for (int i = 0; i < this.sequenceTabs.Length; ++i)
+        {
+            if (i == 0) continue;
+
+            var tab = this.sequenceTabs[i];
+
+            var isUnique = true;
+
+            //loop thru previous tabs
+            for (int j = 0; j < i; ++j)
+            {
+                var prevTab = this.sequenceTabs[j];
+                if (tab.OrderHash == prevTab.OrderHash)
+                    isUnique = false;
+                else if (tab.Numbers[0] == prevTab.Numbers[0])
+                    isUnique = false;
+            }
+
+            while(!isUnique)
+            {
+                randomNums.RandomizeList();
+                tab.SetNumbers(randomNums);
+                isUnique = true;
+
+                //loop thru previous tabs
+                for (int j = 0; j < i; ++j)
+                {
+                    var prevTab = this.sequenceTabs[j];
+                    if (tab.OrderHash == prevTab.OrderHash)
+                        isUnique = false;
+                    else if (tab.Numbers[0] == prevTab.Numbers[0])
+                        isUnique = false;
+                }
+            }
+        }
+        
+        foreach(var tab in this.sequenceTabs)
+        {
+            tab.ApplyNumbers();
         }
     }
 }
--- Assets/Scripts/SequenceTab.cs
+++ Assets/Scripts/SequenceTab.cs
@@ -3,19 +3,40 @@ using UnityEngine;
 
 public class SequenceTab : MonoBehaviour
 {
+    public List<int> Numbers = new();
+    public int OrderHash = 0;
+
     [SerializeField] private Transform[] patterns;
 
     public void SetPatterns(List<int> inPatternOrder)
     {
-        for(int i = 0; i < this.patterns.Length; ++i)
+        for (int i = 0; i < this.patterns.Length; ++i)
         {
             var pattern = this.patterns[i];
             var shouldShow = i < inPatternOrder.Count;
 
             pattern.gameObject.SetActive(shouldShow);
 
-            if(shouldShow)
+            if (shouldShow)
                 pattern.SetSiblingIndex(inPatternOrder[i]);
         }
+    }
+
+    public void SetNumbers(List<int> inPatternOrder)
+    {
+        this.Numbers = new List<int>(inPatternOrder);
+
+        this.OrderHash = 0;
+
+        for(int i = 0; i < this.Numbers.Count; ++i)
+        {
+            this.OrderHash += this.Numbers[i] * (int)Mathf.Pow(10, 4 - i);
+            if (i == 5) break;
+        }
+    }
+
+    public void ApplyNumbers()
+    {
+        SetPatterns(this.Numbers);
     }
 }
